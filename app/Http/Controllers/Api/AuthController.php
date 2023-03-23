f8cdc4f4fb0097e9a8c79dad9eeb8c03271a3a70
Kadir Erman <kadirermantr@gmail.com>
2023-03-23 03:01:41 +0300

    refactor: change directory name

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -11,22 +11,18 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    /**
-     * @param Request $request
-     * @return JsonResponse
-     */
     public function register(Request $request): JsonResponse
     {
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|confirmed'
+            'password' => 'required|string|confirmed',
         ]);
 
         $user = new User([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->password)
+            'password' => bcrypt($request->password),
         ]);
 
         $user->save();
@@ -34,10 +30,6 @@ class AuthController extends Controller
         return response()->json(['message' => 'Successfully created user!'], 201);
     }
 
-    /**
-     * @param Request $request
-     * @return JsonResponse
-     */
     public function login(Request $request): JsonResponse
     {
         $credentials = $request->validate([
@@ -45,23 +37,20 @@ class AuthController extends Controller
             'password' => ['required'],
         ]);
 
-       if (Auth::attempt($credentials)) {
-           $user = Auth::user();
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
 
-           return response()->json([
-               'token' => $user->createToken('login')->accessToken,
-               'token_type' => 'Bearer',
-               'expires_at' => Carbon::parse(Carbon::now()->addWeek())->toDateTimeString(),
-               'success' => 'Successfully logged in',
-           ]);
-       }
+            return response()->json([
+                'token' => $user->createToken('login')->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(Carbon::now()->addWeek())->toDateTimeString(),
+                'success' => 'Successfully logged in',
+            ]);
+        }
 
-       return response()->json(['error' => 'Unauthorized'], 401);
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 
-    /**
-     * @return JsonResponse
-     */
     public function logout(): JsonResponse
     {
         if (Auth::check()) {
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
